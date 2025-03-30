caae419d443a9425007e8a946a5799559fc4301c
meiyujack <meiyujack@msn.cn>
2025-03-31 00:28:11 +0800

    bootstrap5 has been ready and need continue

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members'
+    'members',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
