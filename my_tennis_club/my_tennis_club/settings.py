4824842bce616d49b4ec39575358c8366a645250
meiyujack <meiyujack@msn.cn>
2025-03-30 21:52:46 +0800

    fix last commit

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -27,6 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+SECRET_KEY=config('SECRET_KEY')
 
 # Application definition
 
@@ -78,11 +79,11 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':config['DB_NAME'],
-        'USER':config['DB_USER'],
-        'PASSWORD':config['DB_PASSWORD'],
-        'HOST':config['DB_HOST'],
-        'PORT':config['DB_PORT']
+        'NAME':config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'),
+        'PORT':config('DB_PORT')
     }
 }
 
